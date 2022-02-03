ffd2006a3cc42a7cdb2f3fbf880790fa7081d13c
vrajdesai78 <vrajdesai78@gmail.com>
2022-02-03 16:22:55 +0530

    Routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial/home_page.dart';
+import 'package:tutorial/screens/home_page.dart';
+import 'package:tutorial/screens/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
