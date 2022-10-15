1e1883a1e092af03a8b43d715c2addedc158a678
Rahul <qa@iceipts.com>
2022-10-15 16:01:08 +0530

    [FEAT] added support for all android versions

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial/screens/home_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-            "Login Page",
+    return Material(
+        child: Column(
+      children: [
+        Text("Login Page",
             style: TextStyle(
               fontSize: 20,
               color: Colors.blue,
               fontWeight: FontWeight.bold,
-            )
+            )),
+        SizedBox(
+          height: 200,
         ),
-      ),
-    );
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => HomePage()));
+          },
+          child: Text("Login",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              )),
+        )
+      ],
+    ));
   }
 }
